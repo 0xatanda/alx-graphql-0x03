d37b18d779718274be3c596895b3c9204feabb8d
0xatanda <atanda0x@gmail.com>
2026-01-02 09:44:15 +0100

    feat: feat: add ErrorBoundary component for global error handling

---
+++ alx-rick-and-morty-app/components/ErrorProneComponent.tsx
@@ -0,0 +1,10 @@
+import ErrorProneComponent from "../components/ErrorProneComponent";
+import ErrorBoundary from "../components/ErrorBoundary";
+
+export default function Home() {
+  return (
+    <ErrorBoundary>
+      <ErrorProneComponent />
+    </ErrorBoundary>
+  );
+}
